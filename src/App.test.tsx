c797bf69a9e0262f6c864d1767326df23411cd0a
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-06-28 13:01:16 +0300

    fix error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,31 +1,18 @@
 import MainApp from './App';
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 
-// Object.defineProperty(window, 'matchMedia', {
-//     writable: true,
-//     value: jest.fn().mockImplementation(query => ({
-//         matches: false,
-//         media: query,
-//         onchange: null,
-//         addListener: jest.fn(), // deprecated
-//         removeListener: jest.fn(), // deprecated
-//         addEventListener: jest.fn(),
-//         removeEventListener: jest.fn(),
-//         dispatchEvent: jest.fn()
-//     }))
-// })
-
-// describe("Test", () => {
-//     beforeAll(() => {  
-//       Object.defineProperty(window, "matchMedia", {
-//         value: jest.fn(() => { return { matches: true } })
-//       });
-//     });
-//   });
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {}
+    };
+};
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<MainApp />, div);
     ReactDOM.unmountComponentAtNode(div);
-});
+  });
+    
