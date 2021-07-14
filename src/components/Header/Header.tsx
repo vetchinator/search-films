31796d7b06ed75447226c85c0077bd1dfc371f1b
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-07-14 15:42:17 +0300

    Add StyleComp in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import style from './Header.module.css';
+import styled from 'styled-components/macro';
+
+const HeaderContainer = styled.header`
+    text-align: center;
+    color: #0f135e;
+    background-color: black;
+
+    & h1 {
+        margin: 0;
+        color: aliceblue;
+    }
+    
+`;
 
 const Header: React.FC = () => {
     return (
-        <header className={style.header} >
+        <HeaderContainer>
             <h1>Search movies</h1>
-        </header>
+        </HeaderContainer>
     )
 }
 
