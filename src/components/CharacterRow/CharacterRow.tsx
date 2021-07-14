0899958095d8e0e01e07fc6837bcc49ac6a95e52
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-07-14 15:39:38 +0300

    Add styled components in CharacterRow

--- src/components/CharacterRow/CharacterRow.tsx
+++ src/components/CharacterRow/CharacterRow.tsx
@@ -1,22 +1,52 @@
 import React from 'react';
-import style from './CharacterRow.module.css';
+import styled from 'styled-components/macro';
 
 type PropType = {
     title: string | undefined,
-    value: string,
+    value: string ,
 }
+
+const Row = styled.div`
+    display: flex;
+    text-align: left;
+    padding: 0px 15px;
+    
+    &:not(:first-child) {
+        margin-top: 15px;
+    }
+
+    @media (min-width: 501px) and (max-width: 800px) {
+        width: 80%;
+        justify-content: space-between;
+        margin: 0 auto;
+    }
+
+
+`;
+
+const Title = styled.div`
+    color: grey;
+    width: 40%;
+    color: cadetblue;
+`;
+
+const Value = styled.div`
+    width: 60%;
+    color: midnightblue;
+`;
+
 
 const CharacterRow: React.FC<PropType> = ({ title, value }) => {
 
     return (
-        <div className={style.row}>
-            <div className={style.title}>
+        <Row>
+            <Title>
                 {title}
-            </div>
-            <div className={style.value}>
+            </Title>
+            <Value>
                 {value}
-            </div>
-        </div>
+            </Value>
+        </Row>
     )
 }
 
