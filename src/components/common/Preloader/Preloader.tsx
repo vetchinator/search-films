78ee0f97f29410f9207f91d72ee8c84810a0d1fa
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-07-14 15:42:26 +0300

    Add StyleComp in Preloader

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import "antd/dist/antd.css";
 import { Spin } from "antd";
 import { LoadingOutlined } from "@ant-design/icons";
+import styled from 'styled-components/macro';
 
-const Preloader: React.FC = () => {
+const PreloaderContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+`;
+
+
+const Preloader: React.FC = (props) => {
     const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />;
     return (
-        <div style={{ width: '100%', height: '100%', textAlign: 'center' }}>
+        <PreloaderContainer>
             <Spin indicator={antIcon} />
-        </div>
+        </PreloaderContainer>
     )
 }
 
