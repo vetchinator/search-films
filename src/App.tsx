684938ce6501b9226c05e177f3a6d2252da151d1
Vetchinov Vlad <vlad.vetchinov@gmail.com>
2021-07-14 15:41:56 +0300

    Add StyleComp in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import './App.css';
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import { Provider } from "react-redux";
 import store from "./redux/redux-store";
 import React from 'react';
 import Movies from './components/Movies/Movies';
+import Footer from './components/Footer/Footer';
 import MovieFullDescription from './components/Movies/MovieFullDescription';
+import { BodyContainer } from './components/StyledComponets/StyledComponents';
+import GlobalStyle from './components/StyledComponets/GlobalStyle';
 
 const App: React.FC = () => {
     return (
-        <div className="container">
+        <>
+            <GlobalStyle />
             <Header />
-            <div className="content">
-                    <Switch>
-                        <Route exact path="/" render={() => <Movies />} />
-                        <Route path="/film/:title" render={() => <MovieFullDescription />} />  
-                    </Switch>
-                    
-            </div>
-        </div>
+            <BodyContainer>
+                <Switch>
+                    <Route exact path="/" render={() => <Movies />} />
+                    <Route path="/film/:title" render={() => <MovieFullDescription />} />
+                </Switch>
+            </BodyContainer>
+            <Footer />
+        </>
     );
 }
 
